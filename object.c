cc5ddfb3814b05ede7673b11aa6dbc2bdc854dd4
shravaniprasadjoshi-hub <shravaniprasadjoshi@gmail.com>
2026-04-16 12:32:24 +0530

    Fix: correct object.h include path

--- object.c
+++ object.c
@@ -8,7 +8,7 @@
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions:     object_write, object_read
 
-#include "object.h"
+#include "include/object.h"
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
